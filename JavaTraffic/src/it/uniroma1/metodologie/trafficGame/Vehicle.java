0e2b266ce3113ec9d22fdbb0b9a11d0e88345f9e
scuty2000 <lucascutigliani2@gmail.com>
2020-09-11 05:08:09 +0200

    Added Javadocs pt.2

--- JavaTraffic/src/it/uniroma1/metodologie/trafficGame/Vehicle.java
+++ JavaTraffic/src/it/uniroma1/metodologie/trafficGame/Vehicle.java
@@ -5,34 +5,43 @@ import javafx.scene.shape.Shape;
 import javafx.scene.paint.Color;
 
 /**
- * this is the enum of all the vehicles in the game. Every veihcle has a booean that tells you if the vehicle can turn,
- * a field called shape that contains the shape of the vehicle and a Direction field. this field should be set when the
- * vehicle is spawned.  
+ * This is the Enum of all the vehicles in the game. Every vehicle has a boolean that tells you if the vehicle can turn,
+ * a field called shape that contains the shape of the vehicle and a Direction field.
  */
 
 public enum Vehicle {
 	CAR(true, 60, 40, Color.STEELBLUE),TIR(false ,110, 50, Color.BLACK),MOTORBIKE(true, 37, 16, Color.DARKCYAN);
 	
+	/**
+	 * This indicates if the vehicle
+	 * is able to turn
+	 */
 	private boolean canTurn;
-	
+
 	private int width;
-	
 	private int heigh;
-	
 	private Color c;
-	
+
 	Vehicle(boolean canTurn, int width, int heigh, Color c) {
 		this.canTurn = canTurn;
 		this.width = width;
 		this.heigh = heigh;
 		this.c = c;
 	}
-	
+
+	/**
+	 * This returns the shape of the specific vehicle
+	 * @return
+	 */
 	public Shape getShape() { return new Rectangle(width, heigh, c); }
 	
 	public int getWidth() { return width; }
 	public int getHeigh() { return heigh; }
 	
+	/**
+	 * This returns if the car can move
+	 * @return
+	 */
 	public boolean canTurn() { return canTurn; }
 	
 }
